67634b0e302264075d01a470347e994d302b643f
Mahmud <me@imahmud.com>
2024-08-11 06:22:18 +0600

    Plugin initialize

---
+++ datasheets.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name:       Data Sheets
+ * Plugin URI:        https://github.com/H-Mahmud/datasheets/
+ * Description:       Datasheets Core Plugin enables fast data access with custom storage, adds custom fields in the post editor, and displays content seamlessly on the frontend.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mahmudul Hasan
+ * Author URI:        https://imahmud.com/
+ * Text Domain:       datasheets
+ */
+
+defined('ABSPATH') || exit();
