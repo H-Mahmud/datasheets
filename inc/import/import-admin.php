cfcb672fbee1d9c16d36d1994952c8166df55491
Mahmud <me@imahmud.com>
2024-08-24 13:44:46 +0600

    Localize datasheets script

--- inc/import/import-admin.php
+++ inc/import/import-admin.php
@@ -30,4 +30,12 @@ function datasheets_admin_enqueues($page_slug)
     wp_enqueue_style('datasheets.script', DATASHEETS_DIR_URL . 'assets/datasheets.admin.css', [], '1.0.0', 'all');
     wp_enqueue_media();
     wp_enqueue_script('datasheets.style', DATASHEETS_DIR_URL . 'assets/datasheets.admin.js', ['jquery'], '1.0.0', ['in_footer' => true]);
+    wp_localize_script(
+        'datasheets.script',
+        'Datasheets',
+        array(
+            'ajaxUrl' => admin_url('admin-ajax.php'),
+            'nonce' => wp_create_nonce('_import-csv')
+        )
+    );
 }
