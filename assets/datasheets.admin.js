85e2b23c97ca3a9ab7ba2f8b4d3cafc4a0a9ad90
Mahmud <me@imahmud.com>
2024-08-24 13:24:13 +0600

    Add csv insert form

---
+++ assets/datasheets.admin.js
@@ -0,0 +1,55 @@
+jQuery(document).ready(function ($) {
+    /**
+     * Insert CSV from WordPress Media
+     */
+    $("body").on("click", ".insert-csv", function (e) {
+        e.preventDefault();
+
+        var button = $(this);
+
+        custom_uploader = wp
+            .media({
+                title: "Insert CSV",
+                library: { type: "text/csv" },
+                button: {
+                    text: "Use This CSV",
+                },
+                multiple: false,
+            })
+            .on("select", function () {
+                var attachment = custom_uploader
+                    .state()
+                    .get("selection")
+                    .first()
+                    .toJSON();
+
+                $(button)
+                    .removeClass("button")
+                    .html(
+                        '<span class="dashicons dashicons-media-document"></span>'
+                    )
+                    .next()
+                    .val(attachment.id)
+                    .next()
+                    .show();
+            })
+            .open();
+    });
+
+    /*
+     * Remove CSV which imported from media
+     */
+    $("body").on("click", ".remove-csv", function () {
+        $(".post-type-tr").css("display", "none");
+        $(".mapping-table").html("");
+
+        $(this)
+            .hide()
+            .prev()
+            .val("")
+            .prev()
+            .addClass("button")
+            .html("Upload CSV");
+        return false;
+    });
+});
--- templates/admin/import-data-page.php
+++ templates/admin/import-data-page.php
@@ -1,1 +1,22 @@
-
+<?php defined('ABSPATH') || exit; ?>
+<div class="wrap">
+    <h1><?php echo get_admin_page_title(); ?></h1>
+    <div class="import-container">
+        <h1 class="title">Import Data From CSV</h1>
+        <hr>
+        <form id="import-form">
+            <table class="form-table">
+                <tr>
+                    <th>Insert A CSV File</th>
+                    <td>
+                        <div>
+                            <button class="button insert-csv">Insert CSV</button>
+                            <input type="hidden" required name="csv-file" id="" value="" />
+                            <a href="#" class="remove-csv">Remove CSV</a>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</div>
