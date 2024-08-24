8d331d8ee75d6be6c868fdc169839da324dafde8
Mahmud <me@imahmud.com>
2024-08-24 13:05:52 +0600

    Enqueue admin style

--- inc/import/import-admin.php
+++ inc/import/import-admin.php
@@ -20,3 +20,13 @@ function ds_import_admin_cb()
 {
     include_once DATASHEETS_DIR_PATH . 'templates/admin/import-data-page.php';
 }
+
+add_action('admin_enqueue_scripts', 'datasheets_admin_enqueues');
+function datasheets_admin_enqueues($page_slug)
+{
+    if ($page_slug !== 'data_page_import-data')
+        return;
+
+    wp_enqueue_style('datasheets.script', DATASHEETS_DIR_URL . 'assets/datasheets.admin.css', [], '1.0.0', 'all');
+    wp_enqueue_script('datasheets.style', DATASHEETS_DIR_URL . 'assets/datasheets.admin.js', ['jquery'], '1.0.0', ['in_footer' => true]);
+}
